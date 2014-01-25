833a8f99f91b2cd4d746b4f5ab8e7b81411c2887
oscartan <coffee_oscar@yahoo.com>
2014-01-25 17:33:26 +0800

    menu fix

---
+++ Assets/Scripts/mainmenu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mainmenu : MonoBehaviour
+{
+	
+	public bool isQuit = false; 
+	
+	
+	void OnMouseEnter()
+	{
+		renderer.material.color = Color.red; 
+		
+	}
+	
+	void OnMouseExit() 
+	{
+		renderer.material.color = Color.white; 
+	}
+	
+	void OnMouseDown() 
+	{
+		if(isQuit) 
+		{
+			Application.Quit (); 
+		}
+		
+		else 
+		{
+			Application.LoadLevel(1);
+		}
+		
+	}
+	
+}
