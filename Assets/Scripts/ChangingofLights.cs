6d4f16e3f4ab8a856358e568c07cc3d148932c17
SplashBrother11 <johnmiguel.abejero@benilde.edu.ph>
2014-01-25 08:45:46 +0800

    :)

---
+++ Assets/Scripts/ChangingofLights.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangingofLights : MonoBehaviour  {
+	public bool lightChange;
+	public bool lightChanger;
+	public float resetTimer = 0.5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(lightChange)
+		{
+			light.color = Color.blue;
+				
+		}
+		else if (lightChanger)
+		{
+			light.color = Color.blue;
+
+		}
+		
+	}
+	
+}
