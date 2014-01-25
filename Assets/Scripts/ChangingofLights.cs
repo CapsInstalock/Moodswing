80158d789c90584cfb104efefc74c232deb7dec8
asteriscko <kachw8@yahoo.com>
2014-01-25 11:58:20 +0800

    Added Mood Manager
    Lights also follow the the current mood.

--- Assets/Scripts/ChangingofLights.cs
+++ Assets/Scripts/ChangingofLights.cs
@@ -2,9 +2,9 @@
 using System.Collections;
 
 public class ChangingofLights : MonoBehaviour  {
-	public int lightChange = 1;
+	/*public int lightChange = 1;
 	//public bool lightChanger;
-	public float resetTimer = 0.5f;
+	public float resetTimer = 0.5f;*/  // Not needed
 	// Use this for initialization
 	void Start () {
 		
@@ -13,17 +13,23 @@ public class ChangingofLights : MonoBehaviour  {
 	// Update is called once per frame
 	void Update () 
 	{
-		if(lightChange == 1)
-		{
-			light.color = Color.blue;
+		if(MoodManager.mood == 1) { // References "mood" from MoodManager.cs
+			light.color = Color.magenta;
 				
 		}
-		if (lightChange == 2)
-		{
-			light.color = Color.red;
+		if (MoodManager.mood == 2) {
+			light.color = Color.blue;
 
 		}
-		
+		if (MoodManager.mood == 3) {
+			light.color = Color.red;
+		}
+		if (MoodManager.mood == 4) {
+			light.color = new Color32 (255, 147, 0, 255);
+		}
+		if (MoodManager.mood == 5) {
+			light.color = Color.black;
+		}
 	}
 	
 }
---
+++ Assets/Scripts/MoodManager.cs
@@ -0,0 +1,38 @@
+﻿/* Changes and Contains the moods */
+using UnityEngine;
+using System.Collections;
+
+public class MoodManager : MonoBehaviour {
+
+	public static int mood;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Tenderness")) //Configure in [Edit->Project Settings->Input]
+			mood = 1;
+		else if (Input.GetButtonDown("Sadness"))
+			mood = 2;
+		else if (Input.GetButtonDown("Anger"))
+			mood = 3;
+		else if (Input.GetButtonDown("Excitement"))
+			mood = 4;
+		else if (Input.GetButtonDown("Fear"))
+			mood = 5;
+
+		if (mood == 1)
+			print ("Tender");
+		else if (mood == 2)
+			print ("Sad");
+		else if (mood == 3)
+			print ("Angry");
+		else if (mood == 4)
+			print ("Excitement");
+		else if (mood == 5)
+			print ("Fear");
+	}
+}
