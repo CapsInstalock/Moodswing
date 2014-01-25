dc87451f5b3330d8d16abead829e633bc9e6b102
asteriscko <kachw8@yahoo.com>
2014-01-25 15:39:45 +0800

    Added spawners and temporary objects
    Dosen’t work yet.

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject zero, one, two, three, four, five;
+	public bool activate = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (MoodManager.mood == 0 && activate == false) { // References "mood" from MoodManager.cs
+			Instantiate (zero, this.transform.position, Quaternion.identity);
+			activate = true;
+		}
+		if (MoodManager.mood == 1 && activate == false) { 
+			Instantiate (one, this.transform.position, Quaternion.identity);
+			activate = true;
+		}
+		if (MoodManager.mood == 2 && activate == false) {
+			Instantiate (two, this.transform.position, Quaternion.identity);
+			activate = true;
+		}
+		if (MoodManager.mood == 3 && activate == false) {
+			Instantiate (three, this.transform.position, Quaternion.identity);	
+			activate = true;
+		}
+		if (MoodManager.mood == 4 && activate == false) {
+			Instantiate (four, this.transform.position, Quaternion.identity);
+			activate = true;
+		}
+		if (MoodManager.mood == 5 && activate == false) {
+			Instantiate (five, this.transform.position, Quaternion.identity);
+			activate = true;
+		}
+	}
+}
